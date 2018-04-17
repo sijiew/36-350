f9dee239abb6750435202d14cc0476c0ad99eb27
sijiew <sijiew@andrew.cmu.edu>
2018-04-17 14:46:05 -0400

    Lab12, 2d, restructure functions

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -20,16 +20,28 @@ run_simulation <- function(n_trials, n, p, cutoff) {
     data.vals <- model_select(d$covariates, d$responses, cutoff)
     p.values <- c(p.values, data.vals)
   }
+  save(p.values, file = paste("n-",n,",p-",p,",cut-",cutoff,".RData", sep=""))
+}
+
+make_plot <- function(datapath) {
+  load(datapath)
   hist(x = p.values,
        main = paste("P-Values of n: ", n, ", p: ", p, sep=""),
        xlab = "P-Values",
-       ylab = paste("Frequency in ", n_trials, "trials"))
+       ylab = paste("Frequency in trials"))
 }
 
 par(mfrow = c(3,3))
 cutoff = 0.05
+# Data
 for (n in c(100, 1000, 10000)){
   for (p in c(10, 20, 50)) {
     run_simulation(1000, n, p, cutoff)
+  }
+}
+# Plot
+for (n in c(100, 1000, 10000)){
+  for (p in c(10, 20, 50)) {
+    make_plot(paste("n-",n,",p-",p,",cut-",cutoff,".RData", sep=""))
   }
 }
